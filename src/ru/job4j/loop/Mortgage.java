1c31585f703cf40d8ca783533b277282e0e0c2f2
vnovopashin <novopashinva@gmail.com>
2020-07-10 00:31:02 +0700

    5.7. Ипотека [#318944]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,11 +2,10 @@ package ru.job4j.loop;
 
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
-        int year = 1;
-        double debt = (amount + amount * (percent / 100)) - salary;
+        int year = 0;
 
-        while (debt > 0) {
-            debt = debt - salary;
+        while (amount > 0) {
+            amount = (int) ((amount + amount * (percent / 100)) - salary);
             year++;
         }
         return year;
