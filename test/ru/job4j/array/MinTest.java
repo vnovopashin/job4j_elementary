4c2e113b49c771cd978dcf75af76946edb057a3a
vnovopashin <novopashinva@gmail.com>
2020-07-13 12:37:42 +0700

    6.5.2. Поиск минимума в диапазоне. [#318869]

--- test/ru/job4j/array/MinTest.java
+++ test/ru/job4j/array/MinTest.java
@@ -35,4 +35,37 @@ public class MinTest {
                 is(2)
         );
     }
+
+    @Test
+    public void whenFirstMinInRange13() {
+        assertThat(
+                Min.findMin(
+                        new int[] {-1, 0, 5, 10},
+                        1, 3
+                ),
+                is(0)
+        );
+    }
+
+    @Test
+    public void whenLastMinInRange13() {
+        assertThat(
+                Min.findMin(
+                        new int[] {10, 5, 3, 1},
+                        1, 3
+                ),
+                is(1)
+        );
+    }
+
+    @Test
+    public void whenMiddleMinInRange02() {
+        assertThat(
+                Min.findMin(
+                        new int[] {10, 2, 5, 1},
+                        0, 2
+                ),
+                is(2)
+        );
+    }
 }
