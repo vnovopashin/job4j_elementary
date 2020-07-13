1d3f5d60fdda5e1bb4476594cf835116561cb32f
vnovopashin <novopashinva@gmail.com>
2020-07-13 19:37:00 +0700

    6.6.3. Двухмерный массив. If. [#318882]

---
+++ test/ru/job4j/array/SkipNegativeTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SkipNegativeTest {
+
+    @Test
+    public void skip() {
+        int[][] in = {
+                {1, -2},
+                {1, 2}
+        };
+        int[][] expect = {
+                {1, 0},
+                {1, 2}
+        };
+        int[][] rsl = SkipNegative.skip(in);
+        assertThat(rsl, is(expect));
+    }
+
+    @Test
+    public void skipWhenThree() {
+        int[][] in = {
+                {1, -2, 0},
+                {1, 2, -5}
+        };
+        int[][] expect = {
+                {1, 0, 0},
+                {1, 2, 0}
+        };
+        int[][] rsl = SkipNegative.skip(in);
+        assertThat(rsl, is(expect));
+    }
+}
