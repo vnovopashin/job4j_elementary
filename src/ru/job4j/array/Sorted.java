a302726595950111063237241e324eb9b0c37104
vnovopashin <novopashinva@gmail.com>
2020-09-29 09:10:15 +0700

    1. Проверить сортировку массива Задан числовой массив. Необходимо написать программу, которая проверит, что массив отсортирован по возрастанию. Сортировать массив не нужно!

--- src/ru/job4j/array/Sorted.java
+++ src/ru/job4j/array/Sorted.java
@@ -2,9 +2,12 @@ package ru.job4j.array;
 
 public class Sorted {
     public static boolean isSorted(int[] array) {
-        boolean res = false;
+        boolean res = true;
         for (int i = 1; i < array.length; i++) {
-            res = array[i] >= array[i - 1];
+            if (array[i] < array[i - 1]) {
+                res = false;
+                break;
+            }
         }
         return res;
     }
