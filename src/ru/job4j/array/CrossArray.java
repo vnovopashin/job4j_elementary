009215059ce375cfc769dcf576df01658ed78e01
vnovopashin <novopashinva@gmail.com>
2020-09-30 21:16:31 +0700

    2. Одинаковые элементы для массивов
    Заданы два числовых массива.
    Нужно вывести на консоль числа, которые есть одновременно в первом и втором массиве.

--- src/ru/job4j/array/CrossArray.java
+++ src/ru/job4j/array/CrossArray.java
@@ -1,12 +1,20 @@
 package ru.job4j.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CrossArray {
     public static void printCrossEl(int[] left, int[] right) {
-        for (int l : left) {
-            for (int r : right) {
-                if (l == r) {
-                    System.out.println(l);
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < left.length; i++) {
+            map.put(left[i], 0);
+        }
+        for (int i = 0; i < right.length; i++) {
+            map.put(right[i], map.containsKey(right[i]) ? 1 : 0);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.println(entry.getKey());
             }
         }
     }
